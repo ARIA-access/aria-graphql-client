434a1e2eed2421d0c20e93c047eb15c66f1bdf76
Marcus Povey <marcus@instruct-eric.eu>
2020-11-20 15:05:03 +0000

    Adding outline for site api

---
+++ classes/API/SiteAPI.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ARIA\GraphQLClient\API;
+use ARIA\GraphQLClient\APIDefinition;
+use ARIA\GraphQLClient\Client;
+use ARIA\GraphQLClient\CallException;
+
+class SiteAPI extends APIDefinition {
+
+  /**
+   * Is the currently authenticated user a site administrator for the site.
+   * 
+   * @param string $site_id UUID of the site
+   */
+  public function isAdministrator( string $site_id, string $username ) : bool {
+
+    $mutation = <<< END
+    mutation {
+      isAdministrator(input:{
+        site_id: "$site_id"
+      })
+    }
+    END;
+
+    $result = $this->getClient()->call($mutation, Client::METHOD_POST);
+
+    if (!empty($result['data'])) {
+
+        if ( $result['data']['isAdministrator'] === true ) {
+            return true;
+        }
+        
+    }
+
+    return false;
+  }
+
+}
