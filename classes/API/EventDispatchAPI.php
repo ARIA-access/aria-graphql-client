22b68b7c1abcad01c126272f2f88161859a7b7a1
Marcus Povey <marcus@instruct-eric.eu>
2021-04-30 15:08:40 +0100

    Introducing an event dispatch endpoint

---
+++ classes/API/EventDispatchAPI.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace ARIA\GraphQLClient\API;
+
+use ARIA\GraphQLClient\APIDefinition;
+use ARIA\GraphQLClient\Client;
+use ARIA\GraphQLClient\CallException;
+use ARIA\GraphQLClient\JSONEncodedGQL;
+
+class EventDispatchAPI extends APIDefinition
+{
+  /**
+   * Dispatch an event
+   *
+   * @param string $origin The originating service (e.g. aria-core, profiledata )
+   * @param string $event Type of event, e.g. 'profileUpdate'
+   * @param array $payload Optional payload
+   * @return boolean
+   */
+  public function dispatchEvent(string $origin, string $event, array $payload = []): bool
+  {
+
+    $mutation = "
+      mutation {
+        dispatchEvent(input: {
+          origin: \"$origin\",
+          event: \"$event\",
+          payload: " . JSONEncodedGQL::encode($payload) . "
+        }) {
+            status
+        }
+      }
+      ";
+
+    $result = $this->getClient()->call($mutation, Client::METHOD_POST);
+
+    if (!empty($result['data'])) {
+
+      if ($result['data']['dispatchEvent']['status'] !== 'error') {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
