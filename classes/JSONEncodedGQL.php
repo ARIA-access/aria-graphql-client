94a6208df70561897ec1c02bcd7f0aafd603e044
Marcus Povey <marcus@instruct-eric.eu>
2021-07-29 15:13:11 +0100

    Handle arrays correctly when encoding json

--- classes/JSONEncodedGQL.php
+++ classes/JSONEncodedGQL.php
@@ -8,21 +8,42 @@ namespace ARIA\GraphQLClient;
 class JSONEncodedGQL {
 
     /**
+     * Is the passed array an _associative_ array (object), or a regular array?
+     *
+     * @param array $encode
+     * @return boolean
+     */
+    protected static function isAssoc( array $encode ) : bool {
+        if (array() === $encode) return false;
+        return array_keys($encode) !== range(0, count($encode) - 1);
+    }
+
+    /**
      * Encode an array graphQL compatible format (non-quoted keys)
      */
-    public static function encode( array $encode ) : string {
+    public static function encode( array $encode, bool $showkeys = true, int $depth = 0 ) : string {
 
         $return = '';
-
-        $return .= " { \n";
+        
+        if ($showkeys) {
+            $return .= " { \n";
+        } else {
+            $return .= " [ \n";
+        }
 
         foreach ( $encode as $key => $value ) {
 
-            $return .= " \t {$key}: ";
+
+            for($n = 0; $n < $depth+1; $n++) {
+                $return .= "\t";
+            }
+            if ($showkeys) {
+                $return .= " {$key}: ";
+            }
                 
             if (is_array($value) || is_object($value)) {
 
-                $return .= self::encode( (array) $value);
+                $return .= self::encode( (array) $value, self::isAssoc( (array) $value), $depth+1);
 
             } else {
 
@@ -32,7 +53,14 @@ class JSONEncodedGQL {
             
         }
 
-        $return .= " } \n";
+        for($n = 0; $n < $depth+1; $n++) {
+            $return .= "\t";
+        }
+        if ($showkeys) {
+            $return .= " } \n";
+        } else {
+            $return .= " ] \n";
+        }
 
         return $return;
 
--- tests/JSONEncodedGQLTest.php
+++ tests/JSONEncodedGQLTest.php
@@ -9,16 +9,22 @@ class JSONEncodedGQLTest extends TestCase {
 
   public function testEncode() {
 
+    $obj = new stdClass;
+    $obj->foo = "bar";
+
     $test = [
         'key' => 'value',
         'subkey' => [
             'sub1' => 'val',
-            'sub2' => 5
-        ]
+            'sub2' => 5,
+            'sub3' => $obj,
+        ], 
+        'subarray' => [ 'value', 'value2', 'value3']
         ];
 
     $encoded = JSONEncodedGQL::encode($test);
 
+    echo $encoded;
     $this->assertIsString($encoded);
 
 
