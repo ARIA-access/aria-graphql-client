d83f48e99560731a2e5d8a9e5995f7b06d870780
Marcus Povey <marcus@instruct-eric.eu>
2023-11-01 11:37:14 +0000

    Outlining Mutations

--- classes/API/DataDepositionAPI.php
+++ classes/API/DataDepositionAPI.php
@@ -8,6 +8,7 @@ use ARIA\GraphQLClient\API\Fields\SiteFields;
 use ARIA\GraphQLClient\APIDefinition;
 use ARIA\GraphQLClient\Client;
 use ARIA\GraphQLClient\CallException;
+use ARIA\GraphQLClient\DateTime;
 use ARIA\GraphQLClient\JSONEncodedGQL;
 use RuntimeException;
 
@@ -115,7 +116,127 @@ class DataDepositionAPI extends APIDefinition
     return [];
   }
 
+  /**
+   * Create a bucket.
+   *
+   * @param integer $aria_id
+   * @param string $aria_entity_type
+   * @param integer $embargoed_until
+   * @return array|null
+   */
+  public function createDataBucket(
+    int $aria_id,
+    string $aria_entity_type,
+    int $embargoed_until
+  )
+  {
+    $embargoed_until = DateTime::iso8601Date($embargoed_until);
 
-  // TODO: Mutations
-  
+    $mutation = <<< END
+      mutation {
+        createDataBucket(input: {
+          aria_id: $aria_id,
+          aria_entity_type: "$aria_entity_type",
+          embargoed_until: "$embargoed_until",
+        }) {
+            $this->bucketFields
+        }
+      }
+  END;
+
+    $result = $this->getClient()->call($mutation, Client::METHOD_POST);
+
+    if (!empty($result['data'])) {
+
+      if (!empty($result['data']['createDataBucket'])) {
+        return $result['data']['createDataBucket'];
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Create a storage record in a given bucket.
+   *
+   * @param string $bucket_id
+   * @param string $schema
+   * @return array|null
+   */
+  public function createDataRecord(
+    string $bucket_id,
+    string $schema
+  )
+  {
+   
+    $mutation = <<< END
+      mutation {
+        createDataRecord(input: {
+          bucket: "$bucket_id",
+          schema: "$schema"
+        }) {
+            $this->recordFields
+        }
+      }
+  END;
+
+    $result = $this->getClient()->call($mutation, Client::METHOD_POST);
+
+    if (!empty($result['data'])) {
+
+      if (!empty($result['data']['createDataRecord'])) {
+        return $result['data']['createDataRecord'];
+      }
+    }
+
+    return null;
+  }
+
+  /**
+   * Add a field to a record
+   *
+   * @param string $record_id The record ID
+   * @param string $type Type of field (e.g. URL, Text)
+   * @param array $content Content blob
+   * @param array $options Array of options
+   * @param integer $order Optional ordering value
+   * @return void
+   */
+  public function createDataField(
+    string $record_id,
+    string $type,
+    array $content = [],
+    array $options = [],
+    int $order = 0
+  )
+  {
+    $content = JSONEncodedGQL::encode($content);
+    $options = JSONEncodedGQL::encode($options);
+
+    $mutation = <<< END
+      mutation {
+        createDataField(input: {
+          record: "$record_id",
+          type: "$type",
+          content: "$content",
+          options: "$options",
+          order: $order
+        }) {
+            $this->fieldFields
+        }
+      }
+  END;
+
+    $result = $this->getClient()->call($mutation, Client::METHOD_POST);
+
+    if (!empty($result['data'])) {
+
+      if (!empty($result['data']['createDataField'])) {
+        return $result['data']['createDataField'];
+      }
+    }
+
+    return null;
+  }
+
 }
